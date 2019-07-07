210b21b1d155a854985aecbc0894f464a362f5dd
yykdev <assavictory@gmail.com>
2019-07-07 22:59:46 +0900

    버튼 9개 렌더링

--- square_board_game/src/App.js
+++ square_board_game/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" style={{
+        width: '60px',
+        height: '60px',
+      }}>
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          <Square value={0}/>
+          <Square value={1}/>
+          <Square value={2}/>
+        </div>
+        <div className="board-row">
+          <Square value={3}/>
+          <Square value={4}/>
+          <Square value={5}/>
+        </div>
+        <div className="board-row">
+          <Square value={6}/>
+          <Square value={7}/>
+          <Square value={8}/>
+        </div>
+    </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />
     </div>
   );
 }
