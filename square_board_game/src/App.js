41718b0ed4efdab225c40ea6f147deac2add0b4d
yykdev <assavictory@gmail.com>
2019-07-07 23:39:17 +0900

    승자 계산 함수 추가

--- square_board_game/src/App.js
+++ square_board_game/src/App.js
@@ -2,6 +2,28 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+function checkWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if ( squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+}
+
 function Square (props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -36,6 +58,7 @@ class Board extends React.Component {
       />
     )
   }
+
   render() {
     const status = 'Next Player : ' + (this.state.xIsNext ? 'X' : 'O');
     return (
