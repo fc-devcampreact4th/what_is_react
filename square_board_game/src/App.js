a56061ead9e93ed785acc7c1734ded0536123dbe
yykdev <assavictory@gmail.com>
2019-07-07 23:30:00 +0900

    O/X 체크 기능

--- square_board_game/src/App.js
+++ square_board_game/src/App.js
@@ -1,38 +1,60 @@
 import React from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" style={{
-        width: '60px',
-        height: '60px',
-      }}>
-        {this.props.value}
-      </button>
-    )
-  }
+function Square (props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
+  }
   render() {
+    const status = 'Next Player : ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
+        <div>{status}</div>
         <div className="board-row">
-          <Square value={0}/>
-          <Square value={1}/>
-          <Square value={2}/>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          <Square value={3}/>
-          <Square value={4}/>
-          <Square value={5}/>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          <Square value={6}/>
-          <Square value={7}/>
-          <Square value={8}/>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
     </div>
     )
