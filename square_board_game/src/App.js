858f2372940d1907cb4c974fdf33241cc1b434a1
yykdev <assavictory@gmail.com>
2019-07-08 11:02:38 +0900

    Game 컴포넌트 생성 및 기능 구조 변경

--- square_board_game/src/App.js
+++ square_board_game/src/App.js
@@ -33,53 +33,18 @@ function Square (props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (checkWinner(squares)) {
-      alert('게임이 이미 종료 되었습니다.');
-      return;
-    }
-    if (squares[i]) {
-      alert('이미 체크되었습니다.');
-      return;
-    }
-    squares[i] = this.state.xIsNext? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
 
   render() {
-    const winner = checkWinner(this.state.squares);
-    let status;
-    
-    if (winner) {
-      status = `승자는 : ${winner}`;
-    } else {
-      status = '다음 차례 : ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div>{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -100,10 +65,73 @@ class Board extends React.Component {
   }
 }
 
+class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (checkWinner(squares)) {
+      alert('게임이 이미 종료 되었습니다.');
+      return;
+    }
+    if (squares[i]) {
+      alert('이미 체크되었습니다.');
+      return;
+    }
+    squares[i] = this.state.xIsNext? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares:squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = checkWinner(current.squares);
+
+    let status;
+    
+    if (winner) {
+      status = `승자는 : ${winner}`;
+    } else {
+      status = '다음 차례 : ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <Board />
+      <Game />
     </div>
   );
 }
